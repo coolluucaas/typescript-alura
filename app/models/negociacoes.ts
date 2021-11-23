6e6767456e38908c187947e1192b6fd3ecb66dbd
coolluucaas <lucaspsanta@gmail.com>
2021-11-23 16:20:02 -0300

    simpleficando: declaracao de array tipado

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,14 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = []
+    private negociacoes: Negociacao[] = []
 
     adicionar(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
     }
 
-    listar(): ReadonlyArray<Negociacao> {
-        return [...this.negociacoes]
+    listar(): readonly Negociacao[] {
+        return this.negociacoes
     }
 }
 
