71ad0b69ce7fdf3026f860653091d9718f21fa56
coolluucaas <lucaspsanta@gmail.com>
2021-11-26 12:52:31 -0300

    + access modifiers

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -2,14 +2,9 @@ import { View } from './view.js'
 
 export class MensagemView extends View<string>{ 
 
-    template(model: string): string {
+    protected template(model: string): string {
         return `
         <p class='alert alert-info'>${model}</p>
         `
-    }
-
-    create(model: string): void {
-        const mensagem = this.template(model)
-        this.elemento.innerHTML = mensagem
     }
 }
