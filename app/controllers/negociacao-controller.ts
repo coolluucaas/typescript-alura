45589e064826a3aef451f8c859b19fa346180c49
coolluucaas <lucaspsanta@gmail.com>
2021-12-05 13:48:29 -0300

    definindo dias da semana como enums

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from '../enums/dias-da-semana.js'
 import { Negociacao } from '../models/negociacao.js'
 import { Negociacoes } from '../models/negociacoes.js'
 import { MensagemView } from '../views/mensagem-view.js'
@@ -10,6 +11,7 @@ export class NegociacaoController {
     private negociacoes: Negociacoes = new Negociacoes()
     private negociacoesView = new NegociacoesView('#negociacoesView')
     private mensagemView = new MensagemView('#mensagemView')
+    
 
     constructor() {
         this.inputData = document.querySelector('#data')
@@ -20,13 +22,20 @@ export class NegociacaoController {
 
     public adicionar(): void {
         const negociacao = this.criarNegociacao()
-        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
-            this.negociacoes.adicionar(negociacao)
-            this.atualizarView()
-            this.limparFormulario()
-        } else {
-            this.mensagemView.update('Apenas negociações em dias úteis são aceitas.')
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update(
+                'Apenas negociações em dias úteis são aceitas.'
+            )
+            return
         }
+        this.negociacoes.adicionar(negociacao)
+        this.limparFormulario()
+        this.atualizarView()
+    }
+
+    private ehDiaUtil(data: Date): boolean {
+        return data.getDay() > DiasDaSemana.DOMINGO 
+            && data.getDay() < DiasDaSemana.SABADO
     }
 
     private criarNegociacao(): Negociacao {
