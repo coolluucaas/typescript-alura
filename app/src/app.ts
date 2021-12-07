ee5843b82e1a912711451d4c07d70067b6bfc71d
coolluucaas <lucaspsanta@gmail.com>
2021-12-06 21:15:19 -0300

    + botao importar

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,6 +2,7 @@ import { NegociacaoController } from './controllers/negociacao-controller.js'
 
 const controller = new NegociacaoController()
 const form = document.querySelector('.form')
+const botaoImportar = document.querySelector('#botao-importar')
 if (form) {
     form.addEventListener('submit', (event) => {
         event.preventDefault()
@@ -11,4 +12,14 @@ if (form) {
     throw new Error(
         'Não foi possível inicializar a aplicação. Verifique se o form existe.'
     )
+}
+
+if(botaoImportar){
+    botaoImportar.addEventListener('click', () => {
+        controller.importarDados()
+    })
+
+
+} else {
+    throw new Error('Botão "importar" não foi encontrado.')
 }
