53b2c2ea1873b5cc5bb041d9cc90c11e0c13119b
coolluucaas <lucaspsanta@gmail.com>
2021-12-09 20:03:21 -0300

    alterando de imprimivel de classe para interface

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,13 +1,11 @@
-import { Imprimivel } from "../utils/imprimivel.js"
+import { Imprimivel } from "../interfaces/imprimivel.js"
 
-export class Negociacao extends Imprimivel {
+export class Negociacao implements Imprimivel {
     constructor(
         private _data: Date,
         public readonly quantidade: number,
         public readonly valor: number
-    ) {
-        super()
-    }
+    ) {}
 
     public static Criar(
         inputData: string,
@@ -37,5 +35,11 @@ export class Negociacao extends Imprimivel {
         Quantidade: ${this.quantidade},
         Valor: ${this.valor}
         `
-    }    
+    }  
+    
+    public ehIgual(negociacao:Negociacao): boolean{
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear()
+    }
 }
